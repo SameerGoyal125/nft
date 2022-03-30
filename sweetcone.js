bcfc4d738aa71ab5f3894f40939a5321872d6726
Sameer Goyal <sameergoyal@Sameers-MacBook-Air.local>
2022-03-30 23:47:48 +0530

    Added my project

---
+++ sweetcone.js
@@ -0,0 +1,24 @@
+//jshint esversion:6
+const express= require("express");
+const { reset } = require("nodemon");
+const app = express();
+app.get("/", function(req,res){
+    res.sendFile(__dirname+"/index.html")
+})
+app.get("/faqs", function(req,res){
+    res.sendFile(__dirname+"/faqs.html")
+})
+app.get("/about", function(req,res){
+    res.send(__dirname+"/")
+    res.redirect("/#about")
+
+})
+app.get("/roadmap", function(req,res){
+    res.sendFile(__dirname+"/roadmap.html")
+
+})
+app.use(express.static('public'));
+app.use(express.static('images'));
+app.listen(3000,function(){
+    console.log("Running");
+})
