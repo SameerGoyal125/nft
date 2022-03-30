b3b397eb6054c8519e0c00a91651daa5be83d957
SameerGoyal125 <66504149+SameerGoyal125@users.noreply.github.com>
2022-03-31 01:50:07 +0530

    Update sweetcone.js

--- sweetcone.js
+++ sweetcone.js
@@ -4,21 +4,21 @@ const { reset } = require("nodemon");
 const app = express();
 app.get("/", function(req,res){
     res.sendFile(__dirname+"/index.html")
-})
+});
 app.get("/faqs", function(req,res){
     res.sendFile(__dirname+"/faqs.html")
-})
+});
 app.get("/about", function(req,res){
     res.send(__dirname+"/")
     res.redirect("/#about")
 
-})
+});
 app.get("/roadmap", function(req,res){
     res.sendFile(__dirname+"/roadmap.html")
 
-})
+});
 app.use(express.static('public'));
 app.use(express.static('images'));
-app.listen(3000,function(){
-    console.log("Running");
-})
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
